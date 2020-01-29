a750748f8f6362d796873a1e53cb4d10a9fc34e3
arianna choza <unachoza@gmail.com>
2020-01-29 14:23:39 -0500

    works, with loading spinner

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -15,8 +15,8 @@ const getSeason = (lat, month) => {
   return month > 2 && month < 9 ? (lat > 0 ? 'summer' : 'winter') : (lat < 0 ? 'summer' : 'winter')
 };
 
-const SeasonDisplay = props => {
-  const season = getSeason(props.lat, new Date().getMonth());
+const SeasonDisplay = ({lat}) => {
+  const season = getSeason(lat, new Date().getMonth());
   const { text, iconName } = seasonConfig[season];
 
   return (
--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner'
 
 class App extends React.Component {
-  state = {
-    lat: null,
-    err: null,
-  };
+    state = { lat: null, err: null };
 
-  componentDidMount() {
-    console.log('here');
-    window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.latitude }),
-      err => this.setState({ err: err.message })
-    );
-  }
-
-  componentDidUpdate() {
-    console.log('updated');
-  }
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+        position => this.setState({ lat: position.coords.latitude }),
+        err => this.setState({ err: err.message })
+        );
+    }
+    renderLocationContent = ({ lat, err }) => {
+        return lat ? <SeasonDisplay lat={lat} /> : err ? <div>Error: {err}</div> : <div><Spinner message="please accept location request" /></div>
+    }
 
   render() {
-    const { lat, err } = this.state;
-
-    return lat ? <SeasonDisplay lat={lat} /> : err ? <div>Error: {err}</div> : <div>loading ... </div>;
+      return <div>{this.renderLocationContent(this.state)}</div>
   }
 }
 
