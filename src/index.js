3f7c0d5b589b308708027fffcc6363bb00e3f364
arianna choza <unachoza@gmail.com>
2020-01-29 11:24:42 -0500

    Seasons Display component works with logic and icons

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import SeasonDisplay from './SeasonDisplay';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lat: null,
-      err: null,
-    };
+  state = {
+    lat: null,
+    err: null,
+  };
+
+  componentDidMount() {
+    console.log('here');
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ lat: position.coords.latitude }),
       err => this.setState({ err: err.message })
     );
-  }
-
-  componentDidMount() {
-    console.log('here');
   }
 
   componentDidUpdate() {
@@ -26,7 +23,7 @@ class App extends React.Component {
   render() {
     const { lat, err } = this.state;
 
-    return lat ? <div>Loaction : {lat}</div> : err ? <div>Error: {err}</div> : <div>loading ... </div>;
+    return lat ? <SeasonDisplay lat={lat} /> : err ? <div>Error: {err}</div> : <div>loading ... </div>;
   }
 }
 
