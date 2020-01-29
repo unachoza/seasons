7f9f3c1b1b257821c29d79e6ed4021d94ba70a0b
arianna choza <unachoza@gmail.com>
2020-01-29 10:51:30 -0500

    window geolocatin workin g

--- src/index.js
+++ src/index.js
@@ -1,45 +1,31 @@
-import React from "react"
-import ReactDOM from 'react-dom'
-import SeasonDisplay from './SeasonDisplay'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            lat: null,
-            errorMessage: " "
-        }
-    }
-    componentDidMount(){
-        console.log('Mounted!')
-        window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({ lat : position.coords.latitud }),
-            error => this.setState({errorMessage: error.message})
-            )
-        }
-    componentDidUpdate(){
-        console.log('updated', this.state.lat)
-    }
-    render(){
-        if (this.state.errorMessage && !this.state.lat){
-            return <div>Error: {this.state.errorMessage}</div>
-        }
-        else if  (!this.state.errorMessage && this.state.lat ){
-           return <SeasonDisplay lat={this.state.lat}/>
-        }
-        // else if (this.state.errorMessage && !this.state.lat ){
-        //     return <div>{this.state.errorMessage} </div>
-        
-        return (
-            <div>something{this.state.lat}</div>
-            )
-    }
-   
-    
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: null,
+      errorMessage: ' ',
+    };
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => console.log(err)
+    );
+  }
+
+  componentDidMount() {
+    console.log('here');
+  }
+
+  componentDidUpdate() {
+    console.log('updated');
+  }
+
+  render() {
+    return <div>something{this.state.lat}</div>;
+  }
 }
 
-
-
- 
-
-ReactDOM.render(<App/>, document.querySelector("#root"))
+ReactDOM.render(<App />, document.querySelector('#root'));
