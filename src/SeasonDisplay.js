4dbce5c411258ed1eabb8be886247131ae36ef74
arianna choza <unachoza@gmail.com>
2020-01-29 11:34:01 -0500

    double tiernary

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,37 +1,30 @@
-import React from 'react'
-
+import React from 'react';
 
 const seasonConfig = {
-    summer: {
-        text: "Let's hit the beach!",
-        iconName: 'sun'
-    },
-    winter: {
-        text: "Burr it is cold!",
-        iconName: 'snowflake'
-    }
-}
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: 'sun',
+  },
+  winter: {
+    text: 'Burr it is cold!',
+    iconName: 'snowflake',
+  },
+};
 const getSeason = (lat, month) => {
-    if(month > 2 && month < 9 ){
-      return lat > 0 ? 'summer' : 'winter'
-    } else {
-        return lat > 0 ? 'winter' : 'summer'
-    }
+  return month > 2 && month < 9 ? (lat > 0 ? 'summer' : 'winter') : (lat < 0 ? 'summer' : 'winter')
+};
 
-}
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
 
-const SeasonDisplay = (props) => {
-    const season = getSeason(props.lat, new Date().getMonth())
-    const { text, iconName } = seasonConfig[season]
-    
-    return(
+  return (
     <div>
-        <i className={`${iconName} icon`}/>
-        <h1>{text}</h1> 
-        <i className={`${iconName} icon`}/>
+      <i className={`${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon`} />
     </div>
-        
-    )
-}
+  );
+};
 
-export default SeasonDisplay
+export default SeasonDisplay;
